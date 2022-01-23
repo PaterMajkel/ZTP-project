4fd5f7236224082fbffa2a9310d7e40594f12d5c
michal.boltowicz <micbo26@gmail.com>
2022-01-23 21:05:08 +0100

    game component works

--- ZTP/Client/ZTP-space-invaders/src/app/app.module.ts
+++ ZTP/Client/ZTP-space-invaders/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
-
+import * as Phaser from 'phaser';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,7 @@ import { GameComponent } from './game/game.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
